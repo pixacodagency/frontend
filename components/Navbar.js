463362135444ee5d7d78a66da9df23b73f333080
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-06-25 15:46:18 +0200

    Added 404 page

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
       </div>
       <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
-      <Link href="/blog/"><a>Blog Listing</a></Link>
+      <Link href="/blog/"><a>Blog</a></Link>
     </nav>
 );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,15 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       
+
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
       <Link href="/blog/">
-        <a>See Blog Listing</a>
+        <a className={styles.btn}>See Blog Listing</a>
       </Link>
     </div>
-    
+  
   )
 }
