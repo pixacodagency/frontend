f337a08da8f14b9fe5aa477d0005fd23cebd4267
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-07-01 15:08:57 +0200

    Adde some meta tags

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,15 @@ export default function Home() {
      
     <div>
       <Head>
-       <title>PIXACOD | Home</title>
+      {/*HTML Meta Tags*/}
+       <title>Digital creative agency | Pixacod </title>
+       <meta name="description" content="A digital creative agency specializing in branding, UX/UI design, custom development & ecommerce."></meta>
+       {/*Google / Search Engine Tags*/}
+       <meta itemProp="name" content="Digital creative agency | Pixacod"></meta>
+       <meta itemProp="description" content="A digital creative agency specializing in branding, UX/UI design, custom development & ecommerce."></meta>
+       <meta itemProp="image" content="http://gallery.mailchimp.com/c6baa8aa4b5cc2d85bb8672d8/images/660eea0d-bd7a-4923-af74-eb5513569f1e.png"></meta>
+      {/*Facebbok Meta Tags*/}
+      <meta property="og:url" content="http://pixacod.agency/">
       </Head>
       <h1 className={styles.title}>Get a better UX job</h1>
       <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
