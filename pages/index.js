858ad08d7b157169bcfc775b3a59f418b4248130
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-07-02 13:02:34 +0200

    Updated meta tags

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,20 @@ export default function Home() {
      
     <div>
       <Head>
-      {/*HTML Meta Tags*/}
-       <title>Digital creative agency | Pixacod </title>
-       <meta name="description" content="A digital creative agency specializing in branding, UX/UI design, custom development & ecommerce."></meta>
+      {/*Primary Meta Tags*/}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"></meta>
+       <title>Digital creative agency | Pixacod</title>
+       <meta name="description" content="Full-service creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
        {/*Google / Search Engine Tags*/}
        <meta itemProp="name" content="Digital creative agency | Pixacod"></meta>
-       <meta itemProp="description" content="A digital creative agency specializing in branding, UX/UI design, custom development & ecommerce."></meta>
+       <meta itemProp="description" content="Full-service creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
        <meta itemProp="image" content="http://gallery.mailchimp.com/c6baa8aa4b5cc2d85bb8672d8/images/660eea0d-bd7a-4923-af74-eb5513569f1e.png"></meta>
-      {/*Facebbok Meta Tags*/}
-      <meta property="og:url" content="http://pixacod.agency/">
+      {/*Open Graph / Facebook*/}
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:url" content="https://pixacod.agency/"></meta>
+      <meta property="og:title" content="Digital creative agency | Pixacod"></meta>
+      <meta property="og:description" content="Full-service creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
+      <meta property="og:image" content="http://gallery.mailchimp.com/c6baa8aa4b5cc2d85bb8672d8/images/660eea0d-bd7a-4923-af74-eb5513569f1e.png"></meta>
       </Head>
       <h1 className={styles.title}>Get a better UX job</h1>
       <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
