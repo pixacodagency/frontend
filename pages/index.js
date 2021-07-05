4288b7b080a1beb3e6578df76c074eb3263b22fe
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-07-05 16:47:54 +0200

    Added twitter tags and changes image url

--- pages/index.js
+++ pages/index.js
@@ -14,13 +14,18 @@ export default function Home() {
        {/*Google / Search Engine Tags*/}
        <meta itemProp="name" content="Digital creative agency | Pixacod"></meta>
        <meta itemProp="description" content="Pixacod is a creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
-       <meta itemProp="image" content="/images/meta-image.png"></meta>
+       <meta itemProp="image" content="https://res.cloudinary.com/pixacod-agency/image/upload/v1625495570/website/meta-image_ic13p4.jpg"></meta>
       {/*Open Graph / Facebook*/}
-      <meta property="og:type" content="website"></meta>
       <meta property="og:url" content="https://pixacod.agency/"></meta>
+      <meta property="og:type" content="website"></meta>  
       <meta property="og:title" content="Digital creative agency | Pixacod"></meta>
       <meta property="og:description" content="Pixacod is a creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
-      <meta property="og:image" content="/images/meta-image.png"></meta>
+      <meta property="og:image" content="https://res.cloudinary.com/pixacod-agency/image/upload/v1625495570/website/meta-image_ic13p4.jpg"></meta>
+      {/*Twitter Meta Tags*/}
+      <meta name="twitter:card" content="summary_large_image"></meta>
+      <meta name="twitter:title" content="Digital creative agency | Pixacod"></meta>
+      <meta name="twitter:description" content="Pixacod is a creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
+      <meta name="twitter:image" content="https://res.cloudinary.com/pixacod-agency/image/upload/v1625495570/website/meta-image_ic13p4.jpg"></meta>
       </Head>
       <h1 className={styles.title}>Get a better UX job</h1>
       <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
