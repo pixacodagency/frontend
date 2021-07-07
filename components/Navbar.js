36d20d046bc9a4b8e8acaf4fc43995b15c26be0d
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-07-07 18:11:23 +0200

    Added images cloudinary in config file

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,7 @@ import navStyles from '../styles/Nav.module.css';
 const Navbar = () => {
   return (
     <nav className={navStyles.navigation}>
+      <Image src="https://res.cloudinary.com/pixacod-agency/image/upload/v1625672982/website/logo_hitnt4.png" alt="Image" width={152} height={42}/>
       <ul>
         <li>
           <Link href="/work"><a>Work</a></Link>
--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 module.exports = {  
-  
+  images: {
+    domains:[
+      'res.cloudinary.com'
+    ]
+  },
   eslint: {
-  // Warning: Dangerously allow production builds to successfully complete even if
-  // your project has ESLint errors.
-  ignoreDuringBuilds: true,
-},
+      ignoreDuringBuilds: true,
+   },
   reactStrictMode: true,
 }
