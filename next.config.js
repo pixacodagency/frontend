1580e35d97de149198828f657c3f17c252ad4531
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-07-07 16:39:24 +0200

    Added Google analytics code

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+module.exports = {  
+  
+  eslint: {
+  // Warning: Dangerously allow production builds to successfully complete even if
+  // your project has ESLint errors.
+  ignoreDuringBuilds: true,
+},
   reactStrictMode: true,
 }
--- pages/_document.js
+++ pages/_document.js
@@ -5,13 +5,30 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/*Preloading font*/}
           <link
             rel="preload"
             href="/fonts/gilroy.woff2"
             as="font"
             type="font/woff2"
-            crossOrigin="anonymous"
-          />
+            crossOrigin="anonymous"/>
+          {/* Global Site Tag (gtag.js) - Google Analytics */} 
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+            <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />  
         </Head>
         <body>
           <Main />
