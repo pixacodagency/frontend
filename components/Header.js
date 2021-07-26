e52df11046e40d7903021d9fed04e0b949ca068d
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-07-26 17:39:59 +0200

    Updated hero message

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,12 @@ const Header = () => {
             <Image  src={bannerPic} alt="Banner" height={141} width={1440} />
         </div>
         <div className={headerStyles.container}>
-          <h1>We help move your business forward with design, technology & creativity.</h1>
-          <p className={headerStyles.subhead}>We are a team of creatives who design identities, custom-built WordPress websites, apps and digital experiences for our clients and their customers. </p>
+         <div className={headerStyles.sectionContent}>
+          <div className={headerStyles.contentWrapper}>       
+            <h1>Building digital products, brands, and experiences</h1>
+            <p className={headerStyles.subhead}>We are a team of creatives who design identities, custom-built WordPress websites, apps and digital experiences for our clients and their customers. </p>
+          </div> 
+         </div>
         </div>
       </section>
   )
