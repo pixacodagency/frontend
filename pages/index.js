e3461292e529dc6111c5f359373bedc883e82434
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-07-29 17:29:00 +0200

    Added blog section on home page

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,28 @@ import Button from '../components/Button'
 
 export default function Home({ articles }) {
   return (
-    <div>
-      <h1 className={styles.title}>Welcome to PIXACOD</h1>
-        <ArticleList articles={articles} />
-        <Link href='/posts'>
-          <a>All posts</a>
-        </Link>
-        <br></br>
-      <Button />
-    </div>
+    /*Blog Section*/
+    <section className={`${styles.blogSection} ${styles.blogFaature}`}>
+      <div className={`${styles.container} ${styles.grid} ${styles.News}`}>
+        <div className={styles.newsHeader}>
+         <div className={styles.pageHeader}>
+          <h3 className={styles.bold}>What’s new?<br></br>Important things</h3>
+         </div>
+        </div> 
+        <div className={styles.blog}>
+          <ArticleList articles={articles} />
+          <Link href='/posts'>
+            <a>All posts</a>
+          </Link>
+          <br></br>
+          <Button /> 
+        </div>        
+      </div>  
+    </section>
   )
 }
 export const getStaticProps = async () => {
-const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=3`)
 const articles = await res.json()
  
 
