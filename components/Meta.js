3a1ab078f07b1763f4847ee59601a38602c95933
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-10-12 11:51:05 +0200

    Update meta description text

--- components/Meta.js
+++ components/Meta.js
@@ -11,18 +11,18 @@ const Meta = ({ title, keywords, description }) => {
 		 <link rel='icon' href='/favicon.ico' />
 		 {/*Google / Search Engine Tags*/}
 		 <meta itemProp="name" content="Digital creative agency | Pixacod"></meta>
-		 <meta itemProp="description" content="Pixacod is a creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
+		 <meta itemProp="description" content="Pixacod is a digital agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
 		 <meta itemProp="image" content="https://res.cloudinary.com/pixacod-agency/image/upload/v1625495570/website/meta-image_ic13p4.jpg"></meta>
 		{/*Open Graph / Facebook*/}
 		<meta property="og:url" content="https://pixacod.agency/"></meta>
 		<meta property="og:type" content="website"></meta>  
 		<meta property="og:title" content="Digital creative agency | Pixacod"></meta>
-		<meta property="og:description" content="Pixacod is a creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
+		<meta property="og:description" content="Pixacod is a digital agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
 		<meta property="og:image" content="https://res.cloudinary.com/pixacod-agency/image/upload/v1625495570/website/meta-image_ic13p4.jpg"></meta>
 		{/*Twitter Meta Tags*/}
 		<meta name="twitter:card" content="summary_large_image"></meta>
 		<meta name="twitter:title" content="Digital creative agency | Pixacod"></meta>
-		<meta name="twitter:description" content="Pixacod is a creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
+		<meta name="twitter:description" content="Pixacod is a digital agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce."></meta>
 		<meta name="twitter:image" content="https://res.cloudinary.com/pixacod-agency/image/upload/v1625495570/website/meta-image_ic13p4.jpg"></meta>
     </Head>
   )
@@ -31,7 +31,7 @@ const Meta = ({ title, keywords, description }) => {
 Meta.defaultProps = {
   title: 'Digital creative agency | Pixacod',
   keywords: 'User Experience Design, UX Design, UX Design Agency, Product Design UX, Mobile UX, App UX',
-  description: 'Pixacod is a creative agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce.',
+  description: 'Pixacod is a digital agency specializing in Branding, Digital Marketing, UX/UI Design, Custom Development, & eCommerce.',
 }
  
 export default Meta;
