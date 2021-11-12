b8a31a358468f3f7968ed1972d09773e3a220c73
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-11-12 17:19:40 +0100

    Added a complated 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,26 @@
 import styles from '../styles/404.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
+import bannerPic from '../public/banner-curve.svg'
 
 const NotFoundPage = () => {
 
 	return (  
-		
-		<div className={styles.notFound}>
-			<h1>Ooooops...</h1>
-			<h2>That page cannot be found.</h2>
-			<p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
-		</div>
+		<>
+			<section className={styles.HeroError}>	
+				<div className={styles.curve}>
+					<Image  src={bannerPic} alt="Banner" height={141} width={1440} />
+				</div>
+			</section>
+			<div className={styles.notFound}>		 
+			    <Image  src="/yacht.svg" alt="Yacht" height={152} width={152} /> 
+					<h1 className={styles.display}>Ahoy 404!</h1>
+					<p>It looks like you were travelling in the wrong direction. Hit back or return to home.</p>
+					<Link href="/">
+						<a className={styles.blueBtn}>Return to home</a>
+					</Link>
+				</div>
+		</>
 	);
 }
  
