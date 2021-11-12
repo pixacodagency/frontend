9fa86b90c341cc652779b64b486c7fa391da94fd
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2021-11-12 10:26:49 +0100

    Adde 404 module css

--- pages/404.js
+++ pages/404.js
@@ -1,25 +1,15 @@
 import Link from 'next/link'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 
-const NotFound = () => {
-
-	const router = useRouter()
-	useEffect(() => {
-    setTimeout(() => {
-      router.push('/')
-    }, 3000)
-  }, [])
+const NotFoundPage = () => {
 
 	return (  
 		
-		<div className="not-found">
+		<div className="notFound">
 			<h1>Ooooops...</h1>
 			<h2>That page cannot be found.</h2>
 			<p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
 		</div>
-
 	);
 }
  
-export default NotFound ;
+export default NotFoundPage ;
