abacccf4dd001ff0a4a2f0fdd70bc2db0d914316
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2022-01-13 18:07:01 +0100

    Add new page - expertise

--- pages/expertise.js
+++ pages/expertise.js
@@ -1,13 +1,13 @@
 import Meta from '../components/Meta'
 
-const Store = () => {
+const Expertise = () => {
 	return ( 
 		<div>
-		<Meta title='Store | Pixacod' />
-			<h1>Store PAGE</h1>
+		<Meta title='Expertise | Pixacod' />
+			<h1>EXPERTISE PAGE</h1>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
 		</div>
 	 );
 }
  
-export default Store;
+export default Expertise;
