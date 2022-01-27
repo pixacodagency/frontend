0b3d2526337d391f7e4055e3363b74034d1aefe2
Miroslav Petrović <miroslav.petrovic@domnet.ba>
2022-01-27 10:36:47 +0100

    Add Work Callout component

--- components/WorkCallout.js
+++ components/WorkCallout.js
@@ -5,12 +5,17 @@ import Link from 'next/link';
 const WorkCallout = () => {
 	return ( 
 			<section className={styles.Callout}>
-				<div className={styles.container}>
-					<h2>Got an idea?
-						<Link href="/Contact">
-							<a className={`${styles.specialLink} ${styles.turquoise}`}> Tell us about it</a>
-					  </Link>
-					</h2>
+				<div className={`${styles.container} ${styles.CalloutCointainer}`}>		
+					<div className={styles.CalloutContent}>
+						<div className={styles.CalloutContentBody}>
+							<div className={styles.CalloutMainContent}>	
+								<h2>Got an idea?</h2>							
+								<Link href="/contact">
+									<a className={`${styles.specialLink} ${styles.turquoise} ${styles.linkHasUnderline}`}>Tell us about it</a>
+								</Link>
+							</div>
+						</div>
+					</div>
 				</div>
 			</section>	
 	 );
